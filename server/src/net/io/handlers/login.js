3c52344f2b9cbd6127dac115e7702d1f03005702
Bành Thanh Sơn <takashato@gmail.com>
2019-12-23 22:45:06 +0700

    Update handlers/login.js

--- server/src/net/io/handlers/login.js
+++ server/src/net/io/handlers/login.js
@@ -2,16 +2,18 @@ import Bcrypt from "bcrypt";
 import User from "../../../db/models/user_exported";
 
 export async function doLogin(msg) {
+    console.log('Login: ', msg);
+
     const {username, password} = msg;
 
     if (!username || !password) {
-        this.emit('login response', {success: false, msg: "Vui lòng không bỏ trống tài khoản / mật khẩu."});
+        this.emit('login response', {error: "Vui lòng không bỏ trống tài khoản / mật khẩu."});
         return;
     }
 
     const user = await User.findOne({where: {username: username}});
     if (!user) {
-        this.emit('login response', {success: false, msg: "Tài khoản không tồn tại."});
+        this.emit('login response', {error: "Tài khoản không tồn tại.", errorField: 'username'});
         return;
     }
     if (user.password.length < 50) {
@@ -22,12 +24,14 @@ export async function doLogin(msg) {
         }
     }
     if (!Bcrypt.compareSync(password, user.password)) {
-        this.emit('login response', {success: false, msg: "Sai mật khẩu."});
+        this.emit('login response', {error: "Sai mật khẩu.", errorField: 'password'});
         return;
     }
 
     this.user = user;
-    this.emit('login response', {success: true});
+    this.emit('login response', {
+       user: {...user, password: undefined}
+    });
 }
 
 export async function doRegister(msg) {
