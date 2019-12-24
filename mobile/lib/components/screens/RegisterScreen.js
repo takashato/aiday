c6c2bc3ee97a965a27ed291f425e828877868085
Bành Thanh Sơn <takashato@gmail.com>
2019-12-24 14:05:04 +0700

    Handle register page

--- mobile/lib/components/screens/RegisterScreen.js
+++ mobile/lib/components/screens/RegisterScreen.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import {StyleSheet, SafeAreaView} from 'react-native';
-import {Button, Datepicker, Input, Layout, Text, TopNavigation, TopNavigationAction} from "react-native-ui-kitten";
+import {
+    Button,
+    Datepicker,
+    Input,
+    Layout,
+    Popover, Spinner,
+    Text,
+    TopNavigation,
+    TopNavigationAction
+} from "react-native-ui-kitten";
 import {Icon} from "react-native-eva-icons";
 import moment from "moment";
+import getSocket from "../../net/socketio";
+import {setUser} from "../../redux/actions/user";
+import {connect} from "react-redux";
 
 const ArrowBackIcon = (style) => (<Icon {...style} name="arrow-back"/>);
 
 class RegisterScreen extends React.Component {
     static navigationOptions = {header: null};
-    static minYear = moment().startOf('year').subtract(100,'year');
+    static minYear = moment().startOf('year').subtract(100, 'year');
     static maxYear = moment().startOf('day');
 
     state = {
+        loading: false,
         showPassword: false,
         username: null,
         password: null,
@@ -31,12 +44,54 @@ class RegisterScreen extends React.Component {
 
     toggleShowPassword = () => this.setState({showPassword: !this.state.showPassword});
 
+    handleRegister = async (msg) => {
+        console.log(msg);
+        await this.setState({loading: false});
+        if (msg.error) {
+            if (msg.errorField) {
+                await this.setState({[msg.errorField + 'Error']: msg.error})
+            }
+            return;
+        }
+        this.props.setUser(msg.user);
+    };
+
+    doRegister = async () => {
+        const {username, password, display_name, birthday} = this.state;
+        if (!username || username === '') {
+            this.setState({usernameError: "Vui lòng nhập tên tài khoản."});
+            return;
+        }
+        if (!/^([A-z0-9_]+)$/.test(username) || username.length < 5) {
+            this.setState({usernameError: "Tên tài khoản chỉ gồm chữ hoa, chữ thường, số và dài ít nhất 5 ký tự."});
+            return;
+        }
+        if (!password || password === '' || password.length < 5) {
+            this.setState({passwordError: "Vui lòng nhập mật khẩu ít nhất 5 ký tự."});
+            return;
+        }
+        if (!display_name || display_name === '') {
+            this.setState({display_nameError: "Vui lòng nhập tên hiển thị."});
+            return;
+        }
+        if (!birthday) {
+            this.setState({birthdayError: "Vui lòng nhập ngày sinh."});
+            return;
+        }
+        await this.setState({loading: true});
+        getSocket().emit('register', {
+            username,
+            password,
+            display_name,
+            birthday: moment(birthday).format('DD/MM/YYYY'),
+        });
+        getSocket().on('register response', this.handleRegister);
+    };
+
     render() {
         const passwordEyeIcon = (style) => (
             <Icon {...style} name={this.state.showPassword ? 'eye' : 'eye-off'}/>
         );
-
-        console.log(this.minYear);
 
         return (
             <SafeAreaView style={{flex: 1}}>
@@ -46,7 +101,11 @@ class RegisterScreen extends React.Component {
                     <Layout style={style.registerForm} level="2">
                         <Input label="Tên tài khoản" placeholder="Gồm chữ hoa, thường, và số."
                                value={this.state.username}
-                               onChangeText={val => this.setState({username: val, usernameError: null})}/>
+                               onChangeText={val => this.setState({username: val, usernameError: null})}
+                               status={this.state.usernameError ? 'danger' : 'basic'}
+                               caption={this.state.usernameError || ''}
+                               disabled={this.state.loading}
+                        />
                         <Input
                             label="Mật khẩu"
                             value={this.state.password}
@@ -55,16 +114,27 @@ class RegisterScreen extends React.Component {
                             secureTextEntry={!this.state.showPassword}
                             onIconPress={this.toggleShowPassword}
                             onChangeText={val => this.setState({password: val, passwordError: null})}
+                            status={this.state.passwordError ? 'danger' : 'basic'}
+                            caption={this.state.passwordError || ''}
+                            disabled={this.state.loading}
                         />
-                        <Input label="Tên hiển thị" placeholder="Nhập họ tên hiển thị" value={this.state.display_name}
-                               onChangeText={val => this.setState({display_name: val, display_nameError: null})}/>
+                        <Input label="Tên hiển thị" placeholder="Nhập họ tên hiển thị"
+                               value={this.state.display_name}
+                               onChangeText={val => this.setState({display_name: val, display_nameError: null})}
+                               status={this.state.display_nameError ? 'danger' : 'basic'}
+                               caption={this.state.display_nameError || ''}
+                               disabled={this.state.loading}
+                        />
                         <Text category="c1" appearance="hint" style={{marginBottom: 4}}>Ngày sinh</Text>
                         <Datepicker date={this.state.birthday}
                                     onSelect={date => this.setState({birthday: date, birthdayError: null})}
                                     min={RegisterScreen.minYear.toDate()}
                                     max={RegisterScreen.maxYear.toDate()}
+                                    status={this.state.birthdayError ? 'danger' : 'basic'}
+                                    disabled={this.state.loading}
                         />
-                        <Button status="success" style={{marginTop: 10}}>Đăng ký</Button>
+                        <Button status="success" style={{marginTop: 10}} onPress={this.doRegister}
+                                disabled={this.state.loading}>Đăng ký</Button>
                     </Layout>
                 </Layout>
             </SafeAreaView>
@@ -92,4 +162,10 @@ const style = StyleSheet.create({
     },
 });
 
-export default RegisterScreen;
+const mapDispatchToProps = dispatch => {
+    return {
+        setUser: user => dispatch(setUser(user)),
+    };
+};
+
+export default connect(null, mapDispatchToProps)(RegisterScreen);
--- server/src/net/io/handlers/login.js
+++ server/src/net/io/handlers/login.js
@@ -1,5 +1,7 @@
 import Bcrypt from "bcrypt";
 import User from "../../../db/models/user_exported";
+import moment from "moment";
+import JWT from 'jsonwebtoken';
 
 export async function doLogin(msg) {
     console.log('Login: ', msg);
@@ -30,19 +32,91 @@ export async function doLogin(msg) {
 
     this.user = user;
     this.emit('login response', {
-       user: {
-           id: user.id,
-           username: user.username,
-           email: user.email,
-           birthday: user.birthday,
-           display_name: user.display_name,
-           is_admin: user.is_admin,
-           created_at: user.created_at,
-           updated_at: user.updated_at,
-       }
+        user: {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            birthday: user.birthday,
+            display_name: user.display_name,
+            is_admin: user.is_admin,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+        }
     });
 }
 
 export async function doRegister(msg) {
-    register.screen
+    console.log('register', msg);
+    const {username, password, display_name, birthday} = msg;
+    if (!/^([A-z0-9_]+)$/.test(username) || username.length < 5) {
+        this.emit('register response', {
+            error: 'Tên tài khoản chỉ gồm chữ hoa, chữ thường, số và dài ít nhất 5 ký tự.',
+            errorField: 'username'
+        });
+        return;
+    }
+    if (!password || password === '' || password.length < 5) {
+        this.emit('register response', {
+            error: 'Vui lòng nhập mật khẩu ít nhất 5 ký tự.',
+            errorField: 'password'
+        });
+        return;
+    }
+    if (!display_name || display_name === '') {
+        this.emit('register response', {
+            error: 'Vui lòng nhập tên hiển thị.',
+            errorField: 'display_name'
+        });
+        return;
+    }
+    const birthdayObj = moment(birthday, ['DD/MM/YYYY']);
+    if (!birthday || !birthdayObj.isValid()) {
+        this.emit('register response', {
+            error: 'Vui lòng chọn ngày sinh hợp lệ.',
+            errorField: 'birthday'
+        });
+        return;
+    }
+    if ((await User.count({where: {username: username}})) > 0) {
+        this.emit('register response', {
+            error: 'Tên tài khoản đã tồn tại, vui lòng chọn tên khác.',
+            errorField: 'username'
+        });
+        return;
+    }
+    const user = User.build({
+        username,
+        password: Bcrypt.hashSync(password, Bcrypt.genSaltSync()),
+        email: 'user@aiday.com',
+        birthday: birthdayObj.format('YYYY-MM-DD'),
+        is_admin: 0,
+        display_name: display_name,
+    });
+    try {
+        if (!await user.save()) {
+            this.emit('register response', {
+                error: 'Gặp lỗi khi tạo tài khoản.',
+                errorField: 'username'
+            });
+            return;
+        }
+        this.emit('register response', {
+            user: {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                birthday: user.birthday,
+                display_name: user.display_name,
+                is_admin: user.is_admin,
+                created_at: user.created_at,
+                updated_at: user.updated_at,
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        this.emit('register response', {
+            error: 'Gặp lỗi khi tạo tài khoản.',
+            errorField: 'username'
+        });
+    }
 }
